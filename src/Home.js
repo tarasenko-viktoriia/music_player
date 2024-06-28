8faf9ed0642dd08517966f1a9b2c74ace61324aa
victoriataracenko <victoriataracenko0412@gmail.com>
2024-06-28 13:33:08 +0300

    1

---
+++ src/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export default function Home(){
+    return (
+        <div className="home">
+            <div className="sidebar-wrapper-left">
+                <aside className="sidebar-left">
+                    <div>lorefffffffffffffffffffffffffffffffff</div>
+                </aside>
+            </div>
+            <main className="main-container">
+                <div>lorefffffffffffffffffffffffffffffffff</div>
+            </main>
+            <div className="sidebar-wrapper-right">
+                <aside className="sidebar-right">
+                    <div>lorefffffffffffffffffffffffffffffffff</div>
+                </aside>
+            </div>
+        </div>
+    )
+}
