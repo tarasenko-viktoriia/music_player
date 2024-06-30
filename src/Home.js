0bb66cd20ad297ea2c895fddfbc28e2aeed8e870
victoriataracenko <victoriataracenko0412@gmail.com>
2024-06-30 11:48:50 +0300

    1

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import FadeMenu from "./components/sidebar-left-playlists";
+
 
 
 export default function Home(){
@@ -11,6 +13,26 @@ export default function Home(){
                         <img src="../logo.png" alt="Logo"></img>
                         <p>Bits</p>
                     </div>
+                    <div className="sidebar-left-items">
+                        <div>
+                            <div className="sidebar-left-component">
+                                <img src="./image/music.png" alt="Frame 1" />
+                                <a className="sidebar-left-item">
+                                    Library
+                                </a>
+                            </div>
+                            <div className="sidebar-left-component">
+                                <img src="./image/playlist.png" alt="Frame 2" />
+                                <FadeMenu/>
+                            </div>
+                        </div>
+                        <div className="sidebar-left-component">
+                            <img src="./image/log_in.png" alt="Frame 3" />
+                            <a className="sidebar-left-item">
+                                Log In
+                            </a>
+                        </div>
+                    </div>
                 </aside>
             </div>
             <main className="main-container">
@@ -18,7 +40,10 @@ export default function Home(){
             </main>
             <div className="sidebar-wrapper-right">
                 <aside className="sidebar-right">
-                    <div></div>
+                    <div className="log-in-container">
+                        <p>To use all Bits functions, login into your account</p>
+                        <button>Log in</button>
+                    </div>
                 </aside>
             </div>
         </div>
---
+++ src/components/sidebar-left-playlists.js
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Fade from '@mui/material/Fade';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+export default function FadeMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="fade-button"
+        aria-controls={open ? 'fade-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        endIcon={<KeyboardArrowDownIcon/>}
+        >
+        Playlists
+      </Button>
+      <Menu
+        id="fade-menu"
+        MenuListProps={{
+          'aria-labelledby': 'fade-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        TransitionComponent={Fade}
+      >
+        <MenuItem onClick={handleClose}>Playlists 1</MenuItem>
+        <MenuItem onClick={handleClose}>Playlists 2</MenuItem>
+        <MenuItem onClick={handleClose}>Playlists 3</MenuItem>
+      </Menu>
+    </div>
+  );
+}
