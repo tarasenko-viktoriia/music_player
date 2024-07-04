64b80e1b3993dfd07671da3759fd8236365918c0
victoriataracenko <victoriataracenko0412@gmail.com>
2024-07-04 15:00:26 +0300

    1

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,39 @@
+// Home.js
 import React, { useState } from "react";
-import {Router, Route, Switch, Link, Redirect} from 'react-router-dom';
+import { Router, Route, Switch } from "react-router-dom";
 import createHistory from "history/createBrowserHistory";
 import { useSelector } from "react-redux";
 import Menu from "./components/Menu";
-import { MusicList} from "./data";
+import { MusicList } from "./data";
 import Song from "./components/Song";
 import Playlist from "./components/Playlist";
 import Player from "./components/Player";
 import Basic from "./components/Basic";
+import LoginDialog from "./components/LoginDialog";
+import SignupDialog from "./components/SignupDialog";
 
 export default function Home() {
     const [isSongs, setIsSongs] = useState(true);
-    const song = useSelector(state => state.song.value);
+    const song = useSelector((state) => state.song.value);
     const [search, setSearch] = useState("");
+    const [loginOpen, setLoginOpen] = useState(false);
+    const [signupOpen, setSignupOpen] = useState(false);
+
+    const handleLoginOpen = () => {
+        setLoginOpen(true);
+    };
+
+    const handleLoginClose = () => {
+        setLoginOpen(false);
+    };
+
+    const handleSignupOpen = () => {
+        setSignupOpen(true);
+    };
+
+    const handleSignupClose = () => {
+        setSignupOpen(false);
+    };
 
     return (
         <div className="home">
@@ -35,12 +56,12 @@ export default function Home() {
                             type="search"
                             placeholder="Search..."
                             value={search}
-                            onChange={(e) => setSearch(e.target.value)}>
-                        </input>
+                            onChange={(e) => setSearch(e.target.value)}
+                        />
                     </div>
                     {isSongs ? (
                         <div className="songs">
-                            {MusicList.filter(data =>
+                            {MusicList.filter((data) =>
                                 data.title.toLowerCase().includes(search.toLowerCase())
                             ).map((item) => (
                                 <Song key={item.id} {...item} />
@@ -55,12 +76,18 @@ export default function Home() {
                 <aside className="sidebar-right">
                     <div className="log-in-container">
                         <p>To use all Bits functions, login into your account</p>
-                        <button>Log in</button>
+                        <button onClick={handleLoginOpen}>Log in</button>
                         {song && <Player />}
                     </div>
                     <Basic />
                 </aside>
             </div>
+            <LoginDialog
+                open={loginOpen}
+                handleClose={handleLoginClose}
+                handleSignupOpen={handleSignupOpen}
+            />
+            <SignupDialog open={signupOpen} handleClose={handleSignupClose} />
         </div>
     );
 }
---
+++ src/components/SignupDialog.js
@@ -0,0 +1,61 @@
+// components/SignupDialog.js
+import React, { useState } from "react";
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    TextField,
+    Button
+} from "@mui/material";
+
+export default function SignupDialog({ open, handleClose }) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSignup = () => {
+        // Додайте тут логіку реєстрації
+        handleClose();
+    };
+
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Sign Up</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Email Address"
+                    type="email"
+                    fullWidth
+                    variant="standard"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <TextField
+                    margin="dense"
+                    label="Password"
+                    type="password"
+                    fullWidth
+                    variant="standard"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <TextField
+                    margin="dense"
+                    label="Confirm Password"
+                    type="password"
+                    fullWidth
+                    variant="standard"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handleSignup}>Sign Up</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
