89188f3123c3c7135ba55c50df633ef424237f26
victoriataracenko <victoriataracenko0412@gmail.com>
2024-07-13 15:51:30 +0300

    1

--- src/components/Song.js
+++ src/components/Song.js
@@ -1,29 +1,40 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setAudioTrack, play } from '../Redux/playerSlice';
+import { pauseAudio, playAudio } from '../Redux/playerSlice';
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import PauseIcon from '@mui/icons-material/Pause';
 
 const Song = ({ id, title, artist }) => {
     const dispatch = useDispatch();
-    const playlist = useSelector(state => state.player.playlist);
+    const isPlaying = useSelector(state => state.player.isPlaying);
 
-    const handleSongClick = () => {
-        const trackIndex = playlist.tracks.findIndex(track => track._id === id);
-        if (trackIndex !== -1) {
-            dispatch(setAudioTrack(trackIndex));
-            dispatch(play());
-        }
+    const handlePause = () => {
+        dispatch(pauseAudio());
+    };
+
+    const handlePlay = () => {
+        dispatch(playAudio());
     };
 
     return (
-        <div className="song" onClick={handleSongClick}>
+        <div className="song">
             <div className="song-container">
                 <div className="name-song-container">
+                    <div className="play-pause-button">
+                        {isPlaying ? (
+                            <PauseIcon onClick={handlePause} />
+                        ) : (
+                            <PlayArrowIcon onClick={handlePlay}/>
+                        )}
+                    </div>
                     <div className="song-info">
                         <div className="song-title">{title}</div>
                         <div className="song-artist">{artist}</div>
                     </div>
                 </div>
             </div>
+
+            
         </div>
     );
 };
