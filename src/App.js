5bc12c627e06f2caf8dcbd9f89a108f15201bf83
victoriataracenko <victoriataracenko0412@gmail.com>
2024-07-13 13:02:55 +0300

    1

--- src/App.js
+++ src/App.js
@@ -9,16 +9,9 @@ const App = () => {
 
     // Пример плейлиста
     const samplePlaylist = {
-        _id: '1',
-        name: 'My Playlist',
-        tracks: [
-            { _id: '1', url: 'path/to/song1.mp3', name: 'Song 1' },
-            { _id: '2', url: 'path/to/song2.mp3', name: 'Song 2' },
-            { _id: '3', url: 'path/to/song3.mp3', name: 'Song 3' }
-        ]
+        tracks: []
     };
 
-    // Установим плейлист при загрузке компонента
     React.useEffect(() => {
         dispatch(setAudioPlaylist(samplePlaylist));
     }, [dispatch]);
