dbfa01df6ab12e22c6a668194f65e3d4cb74304a
victoriataracenko <victoriataracenko0412@gmail.com>
2024-07-13 14:09:15 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PlayerControls from './components/PlayerControls';
-import Playlist from './components/Playlist';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAudioPlaylist } from './Redux/playerSlice';
 import './App.css';
