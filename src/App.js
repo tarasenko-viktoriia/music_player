236c2df632b21429e201f8130fdbf7623be5d47b
victoriataracenko <victoriataracenko0412@gmail.com>
2024-07-16 11:58:55 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
 import PlayerControls from './components/PlayerControls';
 import { useDispatch, useSelector } from 'react-redux';
 import { setAudioPlaylist } from './Redux/playerSlice';
 import './App.css';
 import Menu from './components/Menu';
+import Playlist from "./components/Playlist";
 import Song from './components/Song';
 import Login from "./Login"
 
 const App = () => {
     const dispatch = useDispatch();
     const playlist = useSelector(state => state.player.playlist);
+    const [isSongs, setIsSongs] = useState(true);
+    const [search, setSearch] = useState("");
 
     const samplePlaylist = {
         tracks: []
@@ -28,7 +31,7 @@ const App = () => {
                         <p>Bits</p>
                     </div>
                     <div>
-                        <Menu />
+                        <Menu isSongs={isSongs} setIsSongs={setIsSongs} />
                     </div>
                 </aside>
             </div>
@@ -36,16 +39,23 @@ const App = () => {
                 <div className="hero-container">
                     <div className="input-container">
                         <input
-                            className="search-input"
-                            type="search"
-                            placeholder="Search..."
+                                className="search-input"
+                                type="search"
+                                placeholder="Search..."
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
                         />
                     </div>
+                    {isSongs ? (
                     <div className="playlist">
                         {playlist.tracks.map((track, index) => (
                             <Song key={track._id} id={track._id} title={track.name} artist={track.artist} />
                         ))}
                     </div>
+                    ) : (
+                        <Playlist search={search} />
+                    )}
+
                 </div>
             </main>
             <div className="sidebar-wrapper-right">
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Menu() {
+const Menu = ({ isSongs, setIsSongs }) => {
     return (
         <div className="sidebar-left-items">
-            <div className={`sidebar-left-component`}>
+            <div className="sidebar-left-component">
                 <img src="./image/music.png" alt="Frame 1" />
-                <a className="sidebar-left-item">
+                <Link to="/" className="sidebar-left-item" onClick={() => setIsSongs(true)}>
                     Library
-                </a>
+                </Link>
             </div>
             <div className="sidebar-left-component">
                 <img src="./image/playlist.png" alt="Frame 2" />
-                <a className="sidebar-left-item">
+                <Link to="/playlists" className="sidebar-left-item" onClick={() => setIsSongs(false)}>
                     Playlists
-                </a>
+                </Link>
             </div>
-            <div className={`sidebar-left-component`}>
+            <div className="sidebar-left-component">
                 <img src="./image/log_in.png" alt="Frame 3" />
-                <a className="sidebar-left-item">
+                <a href="#" className="sidebar-left-item">
                     Log In
                 </a>
             </div>
         </div>
     );
-}
+};
+
+export default Menu;
